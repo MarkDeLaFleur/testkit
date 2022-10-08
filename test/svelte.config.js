acb9a0c623f4f0f1937b45dad005b155f347d0c8
Mark <mark.delafleur@gmail.com>
2022-10-08 14:45:42 -0400

    another attempt to get vercel to deploy

--- test/svelte.config.js
+++ test/svelte.config.js
@@ -1,26 +1,15 @@
-
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
-
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
 	preprocess: preprocess(),
 
 	kit: {
-	  // default options are shown
-	  adapter: vercel({
-		// if true, will deploy the app using edge functions
-		// (https://vercel.com/docs/concepts/functions/edge-functions)
-		// rather than serverless functions
-		edge: false,
-  
-		// an array of dependencies that esbuild should treat
-		// as external when bundling functions
-		external: [],
-  
-		// if true, will split your app into multiple functions
-		// instead of creating a single one for the entire app
-		split: false
-	  })
+		adapter: adapter()
 	}
-  };
+};
+
+export default config;
